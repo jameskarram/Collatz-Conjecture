f6eb56ca3d4b77889fa46c5ae027e8e16def9b92
jameskarram <96039129+jameskarram@users.noreply.github.com>
2022-05-27 15:43:56 -0500

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,5 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import csv
 
 # DEFINITION OF FUNCTIONS
+
 
 def collatz_odd(x):
     result = (3 * x) + 1
@@ -28,8 +34,13 @@ while runUserCode:
             x = collatz_even(x)
         elif isEven(x) == False:
             x = collatz_odd(x)
-
         results.append(x)
     print(f" these are the results: {results}")
+    split_lists = [results[x:x+1] for x in range(0, len(results), 1)]
+    collatz_df = pd.DataFrame(results)
+    collatz_df.columns = ['Path']
+    print(collatz_df)
+    plt.plot(collatz_df["Path"])
+    plt.show()
     runUserCode = False
 
